54f09e8d3635a82c566872678cf2ff060297e3af
collins <chibuikemcollins61@gmail.com>
2025-03-05 20:24:13 +0800

    ok

--- api/index.js
+++ api/index.js
@@ -6,9 +6,4 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use("/api", router);
 
-const port = process.env.PORT || 3000;
-server.listen(port, ()=>{
-    console.log(`JSON Server is running on port ${port}`)
-});
-
-
+module.exports = server;
